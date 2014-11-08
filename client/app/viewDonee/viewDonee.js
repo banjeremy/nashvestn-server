080937640318159d22c4f6b8a46392a1d43c6597
Gregg Reece <gregg@indiegallery.com>
2014-11-08 16:25:31 -0600

    Initial qrcode code added

--- client/app/viewDonee/viewDonee.js
+++ client/app/viewDonee/viewDonee.js
@@ -8,3 +8,21 @@ angular.module('nashvestnServerApp')
         controller: 'ViewdoneeCtrl'
       });
   });
+
+
+function create_qrcode(text, typeNumber, errorCorrectLevel) {
+  var qrcode = require('qrcode');
+	var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
+	qr.addData(text);
+	qr.make();
+
+	return qr.createImgTag();
+}
+
+$(document).ready(function(){
+  //var qrValue = jquery('#id').val();
+  var qrValue = '123456789012345678901234';
+
+  $('#qrcode').html(create_qrcode(qrValue, 4, 'H'));
+
+});
